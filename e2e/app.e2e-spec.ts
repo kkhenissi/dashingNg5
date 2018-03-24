61d7d53f93e5d9526e4b5da7367f440aedbb685b
khenissi kamel <kkhenissi@gmail.com>
2018-03-24 17:07:57 +0100

    fixe imports modules

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { AppPage } from './app.po';
+/* import { AppPage } from './app.po';
 
 describe('dashingng5 App', () => {
   let page: AppPage;
@@ -12,3 +12,4 @@ describe('dashingng5 App', () => {
     expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
+ */
